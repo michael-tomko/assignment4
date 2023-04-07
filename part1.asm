9871bb7cb2774124dd0502d7011d901af95c3995
Michael <mjt5552@rit.edu>
2023-04-07 04:16:34 -0400

    Revert "deference part, store end result in edx"
    This reverts commit 0870cf22c3481a92834fd784031077a34aaab5d5.

--- part1.asm
+++ part1.asm
@@ -5,7 +5,7 @@
 .data
 num1 DWORD 0AABBCCDDh
 intA dd 10
-intB dd 20
+intB dd 2
 
 .code
 main PROC C
@@ -16,15 +16,11 @@ mov ebx, offset intB
 mov ecx, [eax]
 add [ebx], ecx
 
-mov eax, offset intA
-mov ebx, offset intB
-mov ecx, [eax]
-mov edx, [ebx]
-add ecx, edx
-mov [eax], ecx
+
 
 
 ret
+
 
 main ENDP
 END
